2421aca39c156227a0af9d8285a0a83691ee1d48
Matheus Geraldi da Silva <mgeraldi@ciandt.com>
2026-02-13 15:23:18 -0300

    Add files via upload

---
+++ Ex4.js
@@ -0,0 +1,6 @@
+        let n1 = parseInt(prompt("Digite um número"))
+        let n2 = parseInt(prompt("Digite outro número"))
+
+        let media = (n1 + n2) / 2
+
+        alert("a média é: " + media)
