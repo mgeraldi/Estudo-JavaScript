f04d81e1d5e660ddc331b64fb2e761ae0d52289a
Matheus Geraldi da Silva <mgeraldi@ciandt.com>
2026-02-22 16:40:19 -0300

    Delete Ex2.js

--- Ex2.js
+++
@@ -1,8 +0,0 @@
-        // Variavel
-        let numero1 = prompt("Digite um número")
-        numero1 = parseFloat(numero1)
-        let numero2 = prompt("Digite outro número")
-        numero2 = parseFloat(numero2)
-
-        alert("A soma dos números é " + (numero1 + numero2))
-        
