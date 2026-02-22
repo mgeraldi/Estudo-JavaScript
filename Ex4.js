cdb8f6bbd1a863396963ecfe9121672cfeb16f05
Matheus Geraldi da Silva <mgeraldi@ciandt.com>
2026-02-22 16:39:59 -0300

    Delete Ex4.js

--- Ex4.js
+++
@@ -1,6 +0,0 @@
-        let n1 = parseInt(prompt("Digite um número"))
-        let n2 = parseInt(prompt("Digite outro número"))
-
-        let media = (n1 + n2) / 2
-
-        alert("a média é: " + media)
